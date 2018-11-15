631efce190ab44c04c1da2daa892984a8878233a
Janno Holm <janno.holm@playtech.com>
2018-11-15 16:13:36 +0200

    fixes to ipad UI

--- TowerProxy/server/server-impl/src/main/java/com/playtech/ptargame4/server/rank/calculator/ScoreCriteria.java
+++ TowerProxy/server/server-impl/src/main/java/com/playtech/ptargame4/server/rank/calculator/ScoreCriteria.java
@@ -2,12 +2,12 @@ package com.playtech.ptargame4.server.rank.calculator;
 
 public enum ScoreCriteria {
 	TOWER_HEALTH("towerHealth", 0.5, 500),
-	BRIDGES_BUILT("bridgesBuilt", 0.1, 50),
-    BRIDGES_DESTROYED("bridgesDestroyed", 0.1, 50),
+	BRIDGES_BUILT("bridgesBuilt", 0.1, 5),
+    BRIDGES_DESTROYED("bridgesDestroyed", 0.1, 5),
     BRIDGE_SOLDIER_ENEMY_KILLS("enemyKills", 0.1, 20),
     BRIDGE_SOLDIER_SAVES("soldierSaves", 0.1, 20),
-    BRIDGE_SOLDIER_DEATH("soldierDeaths", 0.1, 20),
-    BRIDGE_SOLDIER_ENEMY_SAVES("enemySaves", 0.1, 20),;
+    BRIDGE_SOLDIER_DEATH("soldierDeaths", 0.1, 10),
+    BRIDGE_SOLDIER_ENEMY_SAVES("enemySaves", 0.1, -10),;
 
 	private final String key;
 	private final double value;
