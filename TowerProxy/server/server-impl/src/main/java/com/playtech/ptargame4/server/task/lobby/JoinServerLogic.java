5e91f440bf24c10ead4dca688c1d66f3da925d31
Janno Holm <janno.holm@playtech.com>
2019-09-25 19:41:47 +0300

    fix join server

--- TowerProxy/server/server-impl/src/main/java/com/playtech/ptargame4/server/task/lobby/JoinServerLogic.java
+++ TowerProxy/server/server-impl/src/main/java/com/playtech/ptargame4/server/task/lobby/JoinServerLogic.java
@@ -49,7 +49,7 @@ public class JoinServerLogic extends AbstractLogic {
         if (users.size() == 0) {
             // register user
             User.UserType userType = request.getClientType() == JoinServerRequest.ClientType.BOT ? User.UserType.BOT : User.UserType.REGULAR;
-            user = getLogicResources().getDatabaseAccess().getUserDatabase().addUser(request.getName(), request.getEmail(), userType, QrGenerator.generateQr(), request.getInformation());
+            user = getLogicResources().getDatabaseAccess().getUserDatabase().addUser(request.getName(), request.getEmail(), userType, QrGenerator.generateQr(), null);
         } else {
             // user first user
             user = users.iterator().next();
